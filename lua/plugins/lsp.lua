62029ffd3d1a2c678918f373f1730dea907d5f1c
Mattia Dal Ben <matthewdibi@gmail.com>
2026-05-24 15:01:55 +0200

    feat: lsp (experimental)

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+    "neovim/nvim-lspconfig",
+    config = function()
+        -- Based on: https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
+        local signs = { Error = "✖ ", Warn = "⚠", Hint = "➤", Info = "ℹ" }
+        for type, icon in pairs(signs) do
+            local hl = "DiagnosticSign" .. type
+            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+        end
+
+        -- Mappings.
+        -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+        local opts = { noremap=true, silent=true }
+        vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+        vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+        -- nvim-cmp configuration
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+        vim.lsp.enable('clangd')
+}
