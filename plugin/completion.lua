6b6b7a23d9039e2dac3739bb941d9ad1bf88e8bb
Mattia Dal Ben <matthewdibi@gmail.com>
2026-06-02 10:53:12 +0200

    refactor: convert blink configuration

---
+++ plugin/completion.lua
@@ -0,0 +1,32 @@
+vim.pack.add({
+    { src = "https://github.com/saghen/blink.cmp", version = 'v1.10.2' }
+})
+
+require('blink.cmp').setup({
+    -- disable friendly_snippets
+    friendly_snippets = false,
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = {
+        preset = 'default',
+        ['<C-p>'] = { 'select_prev', 'fallback' },
+        ['<C-n>'] = { 'select_next', 'fallback' },
+        ['<tab>'] = { 'select_and_accept', 'fallback' },
+    },
+
+    appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+    },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = false } },
+
+    -- List of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+        default = { 'lsp', 'path', 'buffer' },
+    },
+
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+})
